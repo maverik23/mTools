529dcdcf202cfdf78501168729a91944037f631e
maverik23 <smarco@outlook.com>
2022-09-03 10:43:49 -0500

    added garbage collector

---
+++ app/Console/Commands/GarbageCollector.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+
+class GarbageCollector extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'clean:files';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Limpiar los archivos temporales y de descargas';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $folders = [
+            'firmas_zip',
+            'firmas-tmp',
+            'livewire-tmp',
+            'polizas',
+            'polizas_zip',
+            'soportes',
+            'soportes_zip'
+        ];
+
+        foreach ($folders as $folder) {
+            $files =   Storage::allFiles($folder);
+            Storage::delete($files);
+        }
+    }
+}
